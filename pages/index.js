9ef634c6c91a575c80e793d894935beaa404308a
Saad Ahmad Malik <saadislion@gmail.com>
2024-11-23 15:16:12 +0500

    Added SEO and Meta Tags

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,18 @@
-import { getPostData } from '../lib/posts';
-
-export async function getStaticProps() {
-  const { data, content } = getPostData('hello-world.md');
-
-  return {
-    props: {
-      data,
-      content,
-    },
-  };
-}
-
-export default function Home({ data, content }) {
+import Head from "next/head";
+export default function Home() {
   return (
-    <div>
-      <h1>{data.title}</h1>
-      <p>{data.date}</p>
-      <div dangerouslySetInnerHTML={{ __html: content }} />
-    </div>
+    <>
+      <Head>
+        <title>My Static Site</title>
+        <meta name="description" content="This is a sample static site." />
+        <meta property="og:title" content="My Static Site" />
+        <meta
+          property="og:description"
+          content="This is a sample static site using
+Next.js."
+        />
+      </Head>
+      <h1>Welcome to My Static Site</h1>
+    </>
   );
 }
