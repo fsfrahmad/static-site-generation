bb4b2ca41bfdd14967da99ca12d4e045376aafda
Saad Ahmad Malik <saadislion@gmail.com>
2024-11-23 16:54:30 +0500

    Added image with lazy effect to improve performance

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
         />
       </Head>
       <h1>Welcome to My Static Site</h1>
-      <img src="../public/cat.jpg" alt="Example Image" loading="lazy" />
+      <img src="cat.jpg" alt="Example Image" loading="lazy" />
     </>
   );
 }
